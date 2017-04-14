37a0e63e3f84044922acf3b937ed68b94cb69dfc
Leo Schwarz <mail@leoschwarz.com>
2017-04-14 14:00:46 +0200

    tmp

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,17 @@
+// Apparently this is recommended when using error-chain.
+#![recursion_limit = "1024"]
+
+#[macro_use]
+extern crate error_chain;
 extern crate uuid;
 extern crate sxd_document;
 extern crate sxd_xpath;
 
+pub mod errors {
+    error_chain! {
+
+    }
+}
 pub mod entities;
 
 #[cfg(test)]
