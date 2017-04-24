ec66b055ba9036f86e0ffcd24dce98106a152ec8
Leo Schwarz <mail@leoschwarz.com>
2017-04-24 16:21:24 +0200

    testing

--- src/entities/xpath_reader.rs
+++ src/entities/xpath_reader.rs
@@ -40,24 +40,6 @@ pub struct XPathNodeReader<'d> {
     context: &'d Context<'d>,
 }
 
-// TODO
-/*
-pub type SxdParserError = sxd_document::parser::Error;
-type SxdParserErrors = (usize, Vec<sxd_document::parser::Error>);
-pub type SxdXpathError = sxd_xpath::Error;
-*/
-/*
-#[derive(Debug)]
-pub enum ReadError {
-    XmlParserError(SxdParserError),
-    XmlXpathError(SxdXpathError),
-    InvalidData(String),
-    /// There was an internal error somewhere in our code. If this occurs it is considered a bug
-    /// that should be reported and fixed.
-    InternalError(String),
-}
-*/
-
 fn build_xpath(factory: &Factory, xpath_expr: &str) -> Result<XPath, ReadError> {
     factory
         .build(xpath_expr)?
@@ -65,8 +47,9 @@ fn build_xpath(factory: &Factory, xpath_expr: &str) -> Result<XPath, ReadError> 
 }
 
 impl<'d> XPathStrReader<'d> {
+    // TODO: Once the rest of the code stabilizes consider taking a reference to a context, so it
+    // doesn't have to be created anew for each reader instance.
     pub fn new(xml: &str) -> Result<Self, ReadError> {
-
         Ok(Self {
                context: default_musicbrainz_context(),
                factory: Factory::default(),
@@ -98,6 +81,40 @@ impl<'d> XPathReader<'d> for XPathNodeReader<'d> {
     fn evaluate(&'d self, xpath_expr: &str) -> Result<Value<'d>, ReadError> {
         let xpath = build_xpath(&self.factory, xpath_expr)?;
         xpath.evaluate(self.context, self.node).map_err(ReadError::from)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const DOC_1: &'static str =
+        r#"<?xml version="1.0" encoding="UTF-8"?><root><child name="Hello World"/></root>"#;
+
+    #[test]
+    fn xpath_str_reader() {
+        let reader = XPathStrReader::new(DOC_1).unwrap();
+        assert_eq!(reader.evaluate(".//child/@name").unwrap().string(),
+                   "Hello World".to_string());
+    }
+
+    #[test]
+    fn xpath_node_reader() {
+        use sxd_xpath::Value::Nodeset;
+
+        let str_reader = XPathStrReader::new(DOC_1).unwrap();
+        match str_reader.evaluate(".//child").unwrap() {
+            Nodeset(nodeset) => {
+                let node = nodeset.document_order_first().unwrap();
+                let context = default_musicbrainz_context();
+                let node_reader = XPathNodeReader::new(node, &context).unwrap();
+
+                assert_eq!(node_reader.evaluate(".//@name").unwrap().string(),
+                           "Hello World".to_string());
+            }
+            _ => panic!("Nodeset not found!"),
+        };
+
     }
 }
 
